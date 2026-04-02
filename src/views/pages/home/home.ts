750f76593beafbfcf226492572da6ba1c1d82fc8
bagel <ponomarenko.pavel.web@gmail.com>
2026-04-02 20:37:17 +0300

    fix: clear unusable functional

--- src/views/pages/home/home.ts
+++ src/views/pages/home/home.ts
@@ -1,28 +1,10 @@
-import {createStore, applyBindings, ConsoleLogger} from '@razerspine/runtime';
-
-interface HomeState {}
-
 export class HomePage {
-  private logger = new ConsoleLogger();
-  private readonly state: HomeState;
-
   constructor() {
-    const initialData = {};
-    const {state} = createStore(initialData, () => this.update());
-    this.state = state;
-
     this.init();
   }
 
   private init(): void {
-    this.update();
-    this.logger.success(
-      'MPA Home Page initialized with Class-based reactivity!',
-    );
-  }
-
-  private update(): void {
-    applyBindings(document.body, this.state);
+    console.log('HomePage initialized');
   }
 }
 
